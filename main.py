8283c4cc5c83345057a7195ae60fc375c2698eb0
aishwary2003 <100028509+aishwary2003@users.noreply.github.com>
2023-01-29 17:09:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,4 +6,22 @@ root.state('zoomed')
 root.configure(bg='Black')
 
 
+def play_allegro():
+    allegro_track=Playlist.get(ACTIVE)
+    print(allegro_track[0:-4])
+
+def apprehend_allegro():
+    road = filedialog.askdirectory()
+    if road:
+        os.chdir(road)
+        gaane=os.listdire(road)
+
+        for gaana in gaane:
+            if gaana.endwith(".mp3"):
+                Playlist.insert(END,gaana)
+
+mixer.music.load(Playlist.get(ACTIVE))
+    mixer.music.play()
+
+
 root.mainloop()
