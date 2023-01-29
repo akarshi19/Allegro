80000cea20127199e00e45bf1ea34058ebb84f9d
Akarshi Mathur <93787305+akarshi19@users.noreply.github.com>
2023-01-29 17:04:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,6 @@
-import tkinter as tk
-import time
-from tkinter import filedialog
+from tkinter import *
 import pygame
 
 
-root = tk.Tk()
-root.geometry("200x200")
-
-browsebutton = tk.Button(text = "Browse", command = browse_file)
-browsebutton.pack()
-
-playbutton = tk.Button(text = "Play", command = play_music)
-playbutton.pack()
-
-pausebutton = tk.Button(text = "Pause", command = pause_music)
-pausebutton.pack()
-
-stopbutton = tk.Button(text = "Stop", command = stop_music)
-stopbutton.pack()
 
 root.mainloop()
